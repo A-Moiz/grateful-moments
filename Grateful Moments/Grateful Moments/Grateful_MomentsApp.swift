89b5641bbd8a526d24df2a2703470ab9a29a76da
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-04-25 14:36:01 +0100

    Created iOS Project

---
+++ Grateful Moments/Grateful Moments/Grateful_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grateful_MomentsApp.swift
+//  Grateful Moments
+//
+//  Created by Abdul Moiz on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Grateful_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
