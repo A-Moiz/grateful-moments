920a373f2ef23490baaf6e1873194b93f2b94776
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-04-25 15:08:19 +0100

    Created moment entry view

--- Grateful Moments/Grateful Moments/Grateful_MomentsApp.swift
+++ Grateful Moments/Grateful Moments/Grateful_MomentsApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Grateful_MomentsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            MomentEntryView()
         }
     }
 }
